b92f0609dea48ca603897c9d8381acd9f01e8bc4
Константин Фомин <kosty.fomin98@yandex.ru>
2016-09-23 03:41:55 +0300

    Initial Commit

---
+++ SORTIROVOCHKI/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SORTIROVOCHKI
+//
+//  Created by Константин on 23.09.16.
+//  Copyright © 2016 Константин. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
